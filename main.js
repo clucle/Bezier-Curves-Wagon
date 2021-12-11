d3a28a7a0206efd5cd0803f29f8fcca9a3425c06
clucle <wjdenwls123@gmail.com>
2021-12-11 14:55:03 +0900

    feat: init commit
    Signed-off-by: clucle <wjdenwls123@gmail.com>

---
+++ main.js
@@ -0,0 +1,17 @@
+const c = document.getElementById("board");
+const ctx = c.getContext("2d");
+const width = 600;
+const height = 600;
+let room = null;
+
+function updateBoard() {
+    const backgroundColor = "#ffffff";
+    ctx.fillStyle = backgroundColor;
+    ctx.fillRect(0, 0, width, height);
+}
+
+function init() {
+    updateBoard();
+}
+
+init();
